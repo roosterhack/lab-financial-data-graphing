657395695a0fffb6a9b7478933f9b9b9dd758360
Eddie Chung <designered@gmail.com>
2019-04-18 12:41:10 +0200

    done with basic chart

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,47 @@
 //getting data
-const searchBitcoin = (from, to, currency) => {
+const searchBitcoin = () => {
   axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`).then(response => {
-    console.log(response);
+    const { data } = response;
+    const date = Object.keys(data.bpi);
+    const price = Object.values(data.bpi);
+    console.log(date, price);
+    printCharts(date, price);
   });
 };
+
+const printCharts = (date, price) => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: date,
+      datasets: [
+        {
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          label: "Bitcoin chart",
+          data: price
+        }
+      ]
+    }
+  });
+};
+
+searchBitcoin();
+
+// const getStockInfo = stockName => {
+//   axios
+//     .get(`https://api.iextrading.com/1.0/stock/${stockName}/chart`)
+//     .then(response => {
+//       const { data } = response;
+//       console.log(data);
+//       const labels = data.map(el => el.label);
+//       const prices = data.map(el => el.close);
+
+//       printCharts(labels, prices);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
+
+// getStockInfo('amzn');
