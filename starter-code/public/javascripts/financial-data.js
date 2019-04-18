dff45530397151fc9a5f21942b5e4cba6c891add
Eddie Chung <designered@gmail.com>
2019-04-18 23:27:22 +0200

    added currency change

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,12 +1,24 @@
 //get values form input
 const dateFromInput = document.querySelector(".date-from");
 const dateToInput = document.querySelector(".date-to");
+const currencyInput = document.querySelector("#currency");
 
 //getting data
 const searchBitcoin = () => {
-  const dateFrom = document.querySelector(".date-from").value;
-  const dateTo = document.querySelector(".date-to").value;
+  const dateFrom = dateFromInput.value;
+  const dateTo = dateToInput.value;
   axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`).then(response => {
+    const { data } = response;
+    const date = Object.keys(data.bpi);
+    const price = Object.values(data.bpi);
+
+    printCharts(date, price);
+  });
+};
+// update currency
+const changeCurrency = () => {
+  const currency = currencyInput.value;
+  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`).then(response => {
     const { data } = response;
     const date = Object.keys(data.bpi);
     const price = Object.values(data.bpi);
@@ -36,3 +48,4 @@ searchBitcoin();
 
 dateFromInput.addEventListener("change", searchBitcoin);
 dateToInput.addEventListener("change", searchBitcoin);
+currencyInput.addEventListener("input", changeCurrency);
